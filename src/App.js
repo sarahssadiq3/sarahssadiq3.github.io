7fd0693891db7a6c2be4ee1cbfb6e50fb6e13dd3
sarahssadiq3 <106500452+sarahssadiq3@users.noreply.github.com>
2023-04-29 12:08:05 -0500

    added code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './face.png';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Sarah Sadiq's Website :) Thanks for visiting!!
         </p>
         <a
           className="App-link"
